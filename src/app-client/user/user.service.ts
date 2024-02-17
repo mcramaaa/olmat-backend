e8bd990b3cbfeeb7a530eae77217ae3cf511603b
tegarsaputradev <tegaracs.ti@gmail.com>
2024-02-17 16:52:03 +0700

    update

--- src/app-client/user/user.service.ts
+++ src/app-client/user/user.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { EntityCondition } from 'src/shared/types/entity-condition.type';
 import { Users } from 'src/entities/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,23 +9,8 @@ export class UserService {
   constructor(
     @InjectRepository(Users) private userRepository: Repository<Users>,
   ) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
 
   async findOne(condition: EntityCondition<Users>) {
     return await this.userRepository.findOne({ where: condition });
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
