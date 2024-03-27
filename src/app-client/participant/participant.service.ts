7e8aa5c0d325cb25f50ff8a4401b8ce54b5218ed
tegarsaputradev <tegaracs.ti@gmail.com>
2024-03-27 21:01:04 +0700

    edit participant id

--- src/app-client/participant/participant.service.ts
+++ src/app-client/participant/participant.service.ts
@@ -166,7 +166,7 @@ export class ParticipantService {
         const res = await queryRunner.manager.save(
           queryRunner.manager.create(Participants, {
             id:
-              school.city.region.id +
+              school.city.region.region_code +
               school.degree.id +
               rtrim0('0000', String(+participantCount + 1)),
             name: objParticipant.name,
