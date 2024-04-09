a811fa1616c2e7fa6b56424b02f97daa987f0185
mchramaa <rama28moch@gmail.com>
2024-04-09 19:44:00 +0700

    update seed region

--- src/database/seeds/region/region-seed.service.ts
+++ src/database/seeds/region/region-seed.service.ts
@@ -13,24 +13,24 @@ export class RegionSeedService {
     if (count === 0) {
       await this.repository.query(`
             INSERT INTO regions (id, name, region_code, created_at, created_by, updated_at, updated_by) VALUES
-                ('BNDUG', 'Rayon Bandung', '01', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('CRBON', 'Rayon Cirebon', '03', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('JBDTK', 'Rayon Jabodetabek', '04', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('JMBER', 'Rayon Jember', '05', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('JOGJA', 'Rayon Yogyakarta', '18', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('KDIRI', 'Rayon Kediri', '07', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('KLMTN', 'Rayon Kalimantan', '06', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('LAIN2', 'Rayon Ponorogo', '13', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('LMGAN', 'Rayon Lamongan', '08', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('MDIUN', 'Rayon Madiun', '09', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('MDURA', 'Rayon Madura', '10', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('MLANG', 'Rayon Malang', '11', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('PPBLI', 'Rayon Papua Bali', '12', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('PWKTO', 'Rayon Banyumas', '02', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('SLWSI', 'Rayon Sulawesi', '15', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('SMTRA', 'Rayon Sumatra', '16', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('SRBYA', 'Rayon Surabaya', '17', DEFAULT, DEFAULT, DEFAULT, NULL),
-                ('SRKTA', 'Rayon Semarang', '14', DEFAULT, DEFAULT, DEFAULT, NULL);
+                ('BDG', 'Rayon Bandung', '01', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('CRBN', 'Rayon Cirebon', '03', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('JBDT', 'Rayon Jabodetabek', '04', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('JBR', 'Rayon Jember', '05', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('YGY', 'Rayon Yogyakarta', '18', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('KDR', 'Rayon Kediri', '07', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('KLMT', 'Rayon Kalimantan', '06', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('PNRG', 'Rayon Ponorogo', '13', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('LMG', 'Rayon Lamongan', '08', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('MDN', 'Rayon Madiun', '09', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('MDR', 'Rayon Madura', '10', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('MLG', 'Rayon Malang', '11', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('PPBL', 'Rayon Papua Bali', '12', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('BYMS', 'Rayon Banyumas', '02', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('SLWS', 'Rayon Sulawesi', '15', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('SMTR', 'Rayon Sumatra', '16', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('SBY', 'Rayon Surabaya', '17', DEFAULT, DEFAULT, DEFAULT, NULL),
+                ('SMG', 'Rayon Semarang', '14', DEFAULT, DEFAULT, DEFAULT, NULL);
             `);
     }
   }
--- src/shared/enums/permissions.enum.ts
+++ src/shared/enums/permissions.enum.ts
@@ -1,9 +1,16 @@
 export enum PERMISSIONS {
   SCHOOL = 'school',
   SCHOOL_EDIT = 'school-edit',
+  SCHOOL_ACCEPT = 'school-accept',
   PARTICIPANT = 'participant',
+  PARTICIPANT_EDIT = 'participant-edit',
   TRANSACTION = 'transaction',
+  TRANSACTION_EDIT = 'transaction-edit',
   REGION = 'region',
+  REGION_EDIT = 'region-edit',
+  SETTING = 'setting',
+  USER = 'users',
+  USER_EDIT = 'users-edit',
   ADMIN = 'admin',
   ADMIN_EDIT = 'admin-edit',
 }
